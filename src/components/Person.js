bb987a251090e69e43a39d5d52bd439cdcc2f28e
EricFithian <ef2413@columbia.edu>
2022-09-14 16:15:13 -0400

    Updated to add a Person component

---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+function Person({person}) {
+    return (
+        <>
+            <h1>{person.name}</h1>
+            <h2>{person.title}</h2>
+            <img src={person.image} alt={person.name} />
+        </>
+    )
+}
+
+export default Person;{person}
